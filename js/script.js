cc54f1f772c4a585e00aa144d4eab9eb91a6f1d7
Daniel Grigo <daniel.grigo@freenet.de>
2020-01-26 18:01:58 +0100

    Comments added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-
+/********************************************
+Functions for creating the gallery, searchbar and modals
+********************************************/
+/*
+The JSON data received from a fetch-request to randomuser.me api is mapped to "employee"-objects which are
+more concise and easier to access compared to the raw JSON data. If an error occurs, a message is printed on the screen.
+*/
 const requestedJSON = fetch("https://randomuser.me/api?results=12")
- .then( (data) => data.json())
+ .then( (data) => {
+   if(!data.ok){
+     $("header").hide();
+     $("body").html(`<h2>An error occured: ${status} ${statusText}</h2>`);
+   }
+   return data.json();
+ })
  .then ((parsedData) => {
    const mappedData = parsedData.results.map((employee) => {
      employee =
@@ -20,9 +32,10 @@ const requestedJSON = fetch("https://randomuser.me/api?results=12")
    return mappedData;
  })
  .catch( (error) => {
-   console.error(`An error occured: ${error}`)
+   $("header").hide();
+   $("body").html(`<h2>There was a problem with your request: ${error}</h2>`);
 })
-
+// Waits for the data from the fetch request and then creates the gallery out of the employee-objects
 async function createGallery(){
   const dataArray = await requestedJSON;
   let htmlString = "";
@@ -40,13 +53,6 @@ async function createGallery(){
     </div> `
   })
   $(".gallery").html(htmlString);
-
-  $(".gallery").on("click", ".card", (e) =>{
-    const visibleEmployees = $(".card:visible").toArray();
-    const targetIndex = $(".card:visible").index(e.currentTarget);
-    const targetName = $(e.currentTarget).find(".card-name").text();
-    createModal(targetName, targetIndex, visibleEmployees);
-  });
 }
 
 function createSearchBar(){
@@ -56,21 +62,11 @@ function createSearchBar(){
         <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
     </form>`;
   $(".search-container").html(htmlString);
-
-  $("form").on("submit", (e) =>{
-    e.preventDefault()
-    const input = $(".search-input").val().toLowerCase();
-    $(".card").each( function(){
-      const employeeName = $(this).find(".card-name").text().toLowerCase();
-      if(employeeName.startsWith(input) || employeeName.startsWith(input, employeeName.indexOf(" ")+1) || input == ""){
-        $(this).show();
-    } else {
-        $(this).hide();
-      }
-    });
-  })
 }
-
+/* If an employee card is clicked, a modal window is created dynamically which shows more detailed information.
+Also creates buttons to cycle through the modals of different employees. The prev/next button isn't shown if
+the employee is the first/last one displayed in the gallery (that's what the employeeIndex and array of visibleEmployees are needed for).
+*/
 async function createModal(name, employeeIndex, visibleEmployees) {
   const dataArray = await requestedJSON;
   const clickedEmployee =  dataArray.find(employee => employee.name === name);
@@ -105,7 +101,44 @@ async function createModal(name, employeeIndex, visibleEmployees) {
 
   modalEventListeners(employeeIndex, visibleEmployees);
 }
+// Converts the phone number and the birthdate of the retrieved JSON data into a more readable/presentable way
+function convertString(string, phoneOrBday){
+  if (phoneOrBday === "phone"){
+    const newPhone = string.replace(/\D/g, "")
+    return `(${newPhone.substring(0, 3)}) ${newPhone.substring(3, 6)}-${newPhone.substring(6)}`;
+  }
+  if (phoneOrBday === "birthday"){
+    const newBday = string.slice(0, string.indexOf("T")).replace(/\D/g, "");
+    return `${newBday.substring(4, 6)}/${newBday.substring(6)}/${newBday.substring(0, 4)}`;
+  }
+}
 
+/********************************************
+Event listeners
+********************************************/
+// Click listener for creating modals
+  $(".gallery").on("click", ".card", (e) =>{
+    const visibleEmployees = $(".card:visible").toArray();
+    const targetIndex = $(".card:visible").index(e.currentTarget);
+    const targetName = $(e.currentTarget).find(".card-name").text();
+    createModal(targetName, targetIndex, visibleEmployees);
+  });
+/* Submit listener for the search input field. Matches if either the first or last name of an employee
+starts with the entered character(s) and hides all employee cars whitch aren't matched
+*/
+$(".search-container").on("submit", "form", (e) =>{
+  e.preventDefault()
+  const input = $(".search-input").val().toLowerCase();
+  $(".card").each( function(){
+    const employeeName = $(this).find(".card-name").text().toLowerCase();
+    if(employeeName.startsWith(input) || employeeName.startsWith(input, employeeName.indexOf(" ")+1) || input == ""){
+      $(this).show();
+  } else {
+      $(this).hide();
+    }
+  });
+})
+//The listeners for the prev/next and the "close" button.
 function modalEventListeners(employeeIndex, visibleEmployees){
   $(".modal-close-btn").on("click", () =>{
     $(".modal-container").remove()
@@ -126,16 +159,6 @@ function modalEventListeners(employeeIndex, visibleEmployees){
   });
 }
 
-function convertString(string, phoneOrBday){
-  if (phoneOrBday === "phone"){
-    const newPhone = string.replace(/\D/g, "")
-    return `(${newPhone.substring(0, 3)}) ${newPhone.substring(3, 6)}-${newPhone.substring(6)}`;
-  }
-  if (phoneOrBday === "birthday"){
-    const newBday = string.slice(0, string.indexOf("T")).replace(/\D/g, "");
-    return `${newBday.substring(4, 6)}/${newBday.substring(6)}/${newBday.substring(0, 4)}`;
-  }
-}
-
+//Creating gallery and search bar
 createGallery();
 createSearchBar();
